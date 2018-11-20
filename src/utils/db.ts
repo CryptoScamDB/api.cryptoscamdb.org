071ad4b9f7e7c8f88178fedb3b1017dbe3da7b06
hahnmichaelf <hahn.michael.f@gmail.com>
2018-11-20 13:47:34 -0500

    Added category and subcategory stats to the `/stats` endpoint

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -288,3 +288,43 @@ export const checkDuplicate = async (entry: Entry): Promise<any> => {
     }
     return { duplicate: false, type: 'Valid entry.' };
 };
+
+export const getCategoryStats = async (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const category = [];
+        db.scams.forEach(entry => {
+            if (entry.category) {
+                const blank = category.findIndex(en => en.category === entry.category);
+                if (blank >= 0) {
+                    category[blank].occurrences += 1;
+                } else {
+                    category.push({
+                        category: entry.category,
+                        occurrences: 1
+                    });
+                }
+            }
+        });
+        resolve(category);
+    });
+};
+
+export const getSubCategoryStats = async (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const subcategory = [];
+        db.scams.forEach(entry => {
+            if (entry.subcategory) {
+                const blank = subcategory.findIndex(en => en.subcategory === entry.subcategory);
+                if (blank >= 0) {
+                    subcategory[blank].occurrences += 1;
+                } else {
+                    subcategory.push({
+                        subcategory: entry.subcategory,
+                        occurrences: 1
+                    });
+                }
+            }
+        });
+        resolve(subcategory);
+    });
+};
