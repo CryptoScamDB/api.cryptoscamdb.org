65bd328d5d911ccd8a764d4d64edcafd56f9fd79
blurpesec <hahn.michael.f@gmail.com>
2019-01-26 18:56:59 -0500

    Fix issue with repo

--- src/classes/github.class.ts
+++ src/classes/github.class.ts
@@ -1,5 +1,50 @@
 const github = require('octonode');
-const Repo = require('./repo.class');
+
+export class Repo {
+    client?: any;
+    parent?: any;
+    accessToken?: any;
+    repo?: any;
+
+    constructor(client, repo, accessToken) {
+        this.client = client;
+        this.parent = repo;
+        this.accessToken = accessToken;
+        this.repo = client.repo(repo.full_name);
+    }
+
+    getOwner() {
+        return this.parent.owner.login;
+    }
+
+    getBranch() {
+        return this.parent.default_branch;
+    }
+
+    createNew(path, message, content) {
+        return new Promise((resolve, reject) => {
+            this.repo.createContents(path, message, content, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    delete() {
+        return new Promise(async (resolve, reject) => {
+            await this.repo.destroy((err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+}
 
 export default class Github {
     client?: any;
