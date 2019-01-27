9b0f05c24d293156cf2e25377ade37ce872cdc73
Luit Hollander <luit@hollander.email>
2019-01-27 15:58:35 +0100

    Add tests

---
+++ tests/ens.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as ens from '../src/utils/ensResolve';
+
+describe('ens', () => {
+    it('should correctly resolve michaelhahn.eth', async () => {
+        const result = await ens.resolve('michaelhahn.eth');
+        expect(result).to.equal('0x4d1f9d958afa2e96dab3f3ce7162b87daea39017');
+    })
+        .slow(2000)
+        .timeout(10000);
+});
