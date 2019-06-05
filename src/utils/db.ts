01547552c8b6e7493ee6be122537c00705597b8a
409H <409H@users.noreply.github.com>
2019-06-05 18:55:55 +0100

    Commented code to make it less verbose

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -57,7 +57,7 @@ export const get = (query, data?) => {
 
 export const all = (query, data?) => {
     return new Promise((resolve, reject) => {
-        debug('ALL %s %o', query, data);
+        //debug('ALL %s %o', query, data);
         db.all(query, data, (error, rows) => {
             if (error) {
                 debug('ERROR %s %o', query, data);
@@ -71,7 +71,7 @@ export const all = (query, data?) => {
 
 export const run = (query, data?) => {
     return new Promise((resolve, reject) => {
-        debug('RUN %s %o', query, data);
+        //debug('RUN %s %o', query, data);
         db.run(query, data, function(error) {
             if (error) {
                 debug('ERROR %s %o', query, data);
