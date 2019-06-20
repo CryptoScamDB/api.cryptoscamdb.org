f3d54271c17b733cc07e702a02cccb1f82e9d0ba
Luit Hollander <luit@hollander.email>
2019-06-20 21:52:45 +0200

    Small logging changes

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -57,7 +57,7 @@ export const get = (query, data?) => {
 
 export const all = (query, data?) => {
     return new Promise((resolve, reject) => {
-        //debug('ALL %s %o', query, data);
+        debug('ALL %s %o', query, data);
         db.all(query, data, (error, rows) => {
             if (error) {
                 debug('ERROR %s %o', query, data);
@@ -71,7 +71,7 @@ export const all = (query, data?) => {
 
 export const run = (query, data?) => {
     return new Promise((resolve, reject) => {
-        //debug('RUN %s %o', query, data);
+        debug('RUN %s %o', query, data);
         db.run(query, data, function(error) {
             if (error) {
                 debug('ERROR %s %o', query, data);
@@ -140,7 +140,6 @@ export const readEntries = async (): Promise<void> => {
                     await Object.keys(entry.addresses).map(async coin => {
                         await Promise.all(
                             entry.addresses[coin].map(async (address, key) => {
-                                console.log(address + ' - ' + coin);
                                 await run(
                                     'INSERT OR IGNORE INTO addresses(address, coin, entry) VALUES (?,?,?)',
                                     [address, coin, entry.getID()]
