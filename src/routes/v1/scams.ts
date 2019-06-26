74945e01cf2db073886fa5e611144d7eb00421d8
409H <409H@users.noreply.github.com>
2019-06-26 18:33:08 +0100

    Added optional pagination

--- src/routes/v1/scams.ts
+++ src/routes/v1/scams.ts
@@ -2,9 +2,24 @@ import * as db from '../../utils/db';
 import { Request, Response } from 'express';
 
 export default async (req: Request, res: Response) => {
-    const result = await db.all(
-        "SELECT e.id,e.name,e.url,e.path,e.category,e.subcategory,e.description,e.reporter,a.address,a.coin FROM entries e WHERE e.type='scam'"
-    );
+    // Add a limit and skip (optional)
+    let limit = 0;
+    let skip = 0;
+    if (req.query.limit && req.query.limit > 0) {
+        limit = parseInt(req.query.limit);
+    }
+
+    if (req.query.skip && req.query.skip > 0) {
+        skip = parseInt(req.query.skip);
+    }
+
+    const query =
+        'SELECT e.id,e.name,e.url,e.path,e.category,e.subcategory,e.description,e.reporter' +
+        ' FROM entries e' +
+        " WHERE e.type='scam'" +
+        (limit > 0 ? ' LIMIT ' + [skip, limit].join(',') : '');
+
+    const result = await db.all(query);
     res.json({
         success: true,
         result
