c947f67f00b10ec454d94446a0f4e57c10239602
409H <409H@users.noreply.github.com>
2019-06-26 18:35:53 +0100

    Added logic to check against MAX_SAFE_INTEGER

--- src/routes/v1/scams.ts
+++ src/routes/v1/scams.ts
@@ -5,11 +5,11 @@ export default async (req: Request, res: Response) => {
     // Add a limit and skip (optional)
     let limit = 0;
     let skip = 0;
-    if (req.query.limit && req.query.limit > 0) {
+    if (req.query.limit && req.query.limit > 0 && req.query.limit < Number.MAX_SAFE_INTEGER) {
         limit = parseInt(req.query.limit);
     }
 
-    if (req.query.skip && req.query.skip > 0) {
+    if (req.query.skip && req.query.skip > 0 && req.query.limit < Number.MAX_SAFE_INTEGER) {
         skip = parseInt(req.query.skip);
     }
 
