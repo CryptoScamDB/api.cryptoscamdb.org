28076284519aaa2ec1bec662c40db7dde14e73a2
409H <409H@users.noreply.github.com>
2019-07-02 17:30:33 +0100

    Added UUID to the report - old logic, idk how or when it got removed.

--- src/routes/v1/postReport.ts
+++ src/routes/v1/postReport.ts
@@ -2,8 +2,10 @@ import config from '../../utils/config';
 import * as captcha from '../../utils/gcaptcha';
 import * as slack from '../../utils/slack';
 import { Request, Response } from 'express';
+const uuidv1 = require('uuid/v1');
 
 export default async (req: Request, res: Response) => {
+    const strReportId = uuidv1();
     if (
         config.apiKeys.Google_Captcha &&
         config.apiKeys.Slack_Webhook &&
@@ -11,11 +13,13 @@ export default async (req: Request, res: Response) => {
         req.body.args &&
         req.body.args.captcha
     ) {
+        req.body.args.report_id = strReportId;
         const isValidCaptcha = await captcha.verifyResponse(req.body.args.captcha);
         if (isValidCaptcha) {
             slack.sendReport(req.body);
             res.json({
-                success: true
+                success: true,
+                report_id: strReportId
             });
         } else {
             res.json({
@@ -26,7 +30,8 @@ export default async (req: Request, res: Response) => {
     } else if (config.apiKeys.Slack_Webhook && req.body && req.body.args && req.body.args.captcha) {
         slack.sendReport(req.body);
         res.json({
-            success: true
+            success: true,
+            report_id: strReportId
         });
     } else {
         res.json({
