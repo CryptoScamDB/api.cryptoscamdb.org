66a9feff91231ce83cceb56f564c59e36aadb803
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-10-10 18:18:36 +0100

    Added logic to filter by ticker/coin

--- src/routes/v1/addresses.ts
+++ src/routes/v1/addresses.ts
@@ -2,10 +2,19 @@ import * as db from '../../utils/db';
 import { Request, Response } from 'express';
 
 export default async (req: Request, res: Response) => {
-    const result = {};
-    const addresses: any = await db.all(
+    let query = [
         'SELECT entries.*, addresses.address FROM addresses LEFT JOIN entries ON entries.id = addresses.entry'
-    );
+    ];
+    if (req.query.ticker) {
+        const ticker = req.query.ticker;
+        // Validate and sanitize input
+        if (ticker.match(/^[a-z]{3,5}$/i)) {
+            query.push(`WHERE addresses.coin = '${ticker.toUpperCase()}'`);
+        }
+    }
+
+    const result = {};
+    const addresses: any = await db.all(query.join(' '));
     addresses.forEach(entry => {
         if (!(entry.address in result)) {
             result[entry.address] = [];
