7824ddc2f2bfcf89596d89d7ce0506de54fe9f22
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-12-11 16:03:49 +0000

    Added endpoint to return scam domains in text/plain format separated by newline so people can feed it into firewalls easier

---
+++ src/routes/v1/blacklist/domain.ts
@@ -0,0 +1,16 @@
+import * as db from '../../../utils/db';
+import { Request, Response } from 'express';
+
+export default async (req: Request, res: Response) => {
+    const entry: any = await db.all("SELECT hostname FROM entries WHERE type='scam'");
+
+    let domains: string[] = [];
+
+    entry.map(e => {
+        domains.push(e.hostname);
+    });
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Dispositon', 'inline');
+    res.send(domains.join('\r\n'));
+};
